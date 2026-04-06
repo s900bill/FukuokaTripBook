77ae522a0f7a87cd7c6d0e6346ae30492f53ea9a
bill_hsu <bill_hsu@kingwaytek.com>
2026-04-06 20:44:13 +0800

    pwa: update manifest theme color and service worker cache manifest for Sakura theme

--- sw.js
+++ sw.js
@@ -1,9 +1,24 @@
 // Service Worker 版本號 - 每次更新時請修改這個版本號
-const VERSION = "v1.0.3";
+const VERSION = "v1.1.0";
 const CACHE_NAME = `fukuoka-trip-${VERSION}`;
 
-// 需要緩存的文件
-const urlsToCache = ["./index.html", "./trip.json", "./manifest.json"];
+// 需要緩存的文件 (App Shell)
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./trip.json",
+  "./manifest.json",
+  "./css/style.css",
+  "./js/app.js",
+  "./js/daily.js",
+  "./js/expense.js",
+  "./js/sakura.js",
+  "./js/checklist.js",
+  "./js/japanese.js",
+  "./js/converter.js",
+  "./js/admin.js",
+  "./js/lists.js"
+];
 
 // 安裝 Service Worker
 self.addEventListener("install", (event) => {
